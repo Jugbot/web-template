098030bd9f2af67d07517f7eca01c7c0948fbbfb
Lucas Pollice <lp1935@nyu.edu>
2024-09-13 22:11:46 -0400

    first pass

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,9 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ export default {
   "*": [
     () => "pnpm install",
     "pnpm _prettier --write",
-    "pnpm _eslint --fix",
-    () => "pnpm run types:check",
+    "pnpm _eslint --no-warn-ignored --fix",
+    () => "pnpm run check-types",
   ],
 };
